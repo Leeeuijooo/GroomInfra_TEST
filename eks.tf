d7e320578c05ab5ec10f21b0585005ac91989212
LEE EUI JOO <96514378+Leeeuijooo@users.noreply.github.com>
2023-06-08 10:48:26 +0900

    Additional Security Groups

--- eks.tf
+++ eks.tf
@@ -45,3 +45,12 @@ module "eks" {
     }
   }
 }
+
+resource "aws_security_group_rule" "allow_ingress" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.eks.cluster_security_group_id
+}
