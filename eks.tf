c6e2499a37f31463f94d59eb7161d30d3c7c5472
LEE EUI JOO <96514378+Leeeuijooo@users.noreply.github.com>
2023-06-16 18:23:11 +0900

    EKS 애드온 기능 추가
    ebs-csi-driver
    vpc-cni
    kube-proxy
    coredns

--- eks.tf
+++ eks.tf
@@ -17,6 +17,21 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+    
+  }
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
   }
