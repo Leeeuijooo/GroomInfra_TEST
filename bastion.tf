79fdc2952a8fd2440ac944dc16a67463b5e9c772
LEE EUI JOO <96514378+Leeeuijooo@users.noreply.github.com>
2023-06-20 16:30:33 +0900

    키페어 수정(아이디 교체)
    키페어 수정(아이디 교체)

--- bastion.tf
+++ bastion.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ubuntu_bastion" {
   ami = "ami-0c9c942bd7bf113a2"
   availability_zone = var.azs[0]
   instance_type = "t2.micro"
-  key_name = "Groom"
+  key_name = "grooom"
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   subnet_id = module.vpc.public_subnets[0]
   associate_public_ip_address = true
@@ -12,7 +12,7 @@ resource "aws_instance" "ubuntu_bastion" {
   connection {
     type = "ssh"
     user = "ubuntu"
-    private_key = file("../Groom.pem")
+    private_key = file("../grooom.pem")
     host = self.public_ip
   }
 
